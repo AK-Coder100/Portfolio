8dc57a8391f4c049bb7f95501ce1dab969019966
Abhishek Gupta <abhishek.gupta@a2d.co.in>
2023-06-08 11:14:48 +0530

    removed smtp

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const send = document.getElementById("send")
+// const send = document.getElementById("send")
 
-function send_mail() {
-    //site use is emailjs
-    emailjs.init('ldeGyfVMt8ZoNnLI5')
-    emailjs.send("service_k0av37i", "template_poygk7o", {
-        sub: "Portfolio Mail",
-        from_name: document.getElementById("mail_id").value,
-        message: document.getElementById("msg").value,
-    }).then(() => {
-        alert('Sent!');
-    }, (err) => {
-        alert("Not send !! \n Try again");
-    });;
-}
+// function send_mail() {
+//     //site use is emailjs
+//     emailjs.init('ldeGyfVMt8ZoNnLI5')
+//     emailjs.send("service_k0av37i", "template_poygk7o", {
+//         sub: "Portfolio Mail",
+//         from_name: document.getElementById("mail_id").value,
+//         message: document.getElementById("msg").value,
+//     }).then(() => {
+//         alert('Sent!');
+//     }, (err) => {
+//         alert("Not send !! \n Try again");
+//     });;
+// }
 
 
-send.addEventListener("click", send_mail)
+// send.addEventListener("click", send_mail)
